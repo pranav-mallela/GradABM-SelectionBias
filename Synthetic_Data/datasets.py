05a5b18853427d2f26b3c254872fde9a4134a263
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2024-10-10 00:09:37 -0400

    added epidemic curve to sample points

--- Synthetic_Data/datasets.py
+++ Synthetic_Data/datasets.py
@@ -12,28 +12,41 @@ class SyntheticDataGenerator():
 
     def simulate_dataset(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
-        TODO: write interpretation of data structure.
-        
+        Data: One row represents deidentified patient data
+        Columns: time (in weeks), demographic, region, weekly_hosp_rate, observed (True if sampled, else False)
+
         """
-        t = self.rng.choice(a=np.arange(1, 53), size=self.dataset_size)
-        d = self.rng.choice(a=np.arange(self.num_demographics), size=self.dataset_size)
-        # use delta_r to choose d
-        # use time to sample individual points
+        # Sampling time
+        weekly_df = pd.read_csv("./hybridODE_weekly_data.csv")
+        rel_cols_df = weekly_df[["time", "beta", "Ca", "Infectious_mild", "Infectious_severe", "Hospitalized_recovered", "Hospitalized_deceased"]]
+
+        start = 0
+        timesteps = 52
+        end = start + timesteps
+
+        prob_dist = rel_cols_df["Infectious_severe"][start:end]/rel_cols_df["Infectious_severe"][start:end].sum()
+        t = self.rng.choice(a=52, size=int(1e5), p=prob_dist)
+
+        # Sampling region
         r = self.rng.choice(a=np.arange(self.num_regions), size=self.dataset_size)
 
-        # vector of demographic percentages by region
-        delta_r = self.rng.dirichlet(np.ones(self.num_demographics), size=self.num_regions)
+        # Sampling demographic
+        delta_r = self.rng.dirichlet(np.ones(self.num_demographics), size=self.num_regions) # matrix of demographic percentages by region
+        d = [self.rng.choice(a=np.arange(self.num_demographics), size=self.dataset_size, p=delta_r[region]) for region in r]
 
-        # linear combination of factors influencing hospitalization rate
-        pi_P = 0.01 * t + 0.01 * d + 0.01 * r
-        hospitalization_rate = expit(pi_P)
+        # model hospitalization rate for a dem d during week t 
+        # # = number of hosp. / total pop. in region r
+        # case 1: all demographics have the same weekly hospitalization rate
+        # case 2: different rates per dem
+        pi_P = 0.01 * d
+        weekly_hosp_rate = expit(pi_P)
 
         # simulate a biased dataset using distribution Q
-        pi_Q = expit(0.005 * t + 0.005 * d + 0.005 * r) # ranges from 0 to 0.005 * (t_max + d_max + r_max)
+        pi_Q = expit(0.005 * d)
 
         # indicator variable
         obs = pi_Q > self.rng.uniform(size=self.dataset_size)
 
         X_total = np.stack((t, d, r), axis=-1)
 
-        return X_total, hospitalization_rate, obs, delta_r
+        return X_total, weekly_hosp_rate, obs, delta_r
